3faa6c86377494c2a1cd305d43b02ec15cc64875
tame12 <36912595+tame12@users.noreply.github.com>
2021-09-22 10:14:37 +0800

    removed NOT NULL

--- database/internFYI.sql
+++ database/internFYI.sql
@@ -14,22 +14,23 @@ DROP TABLE IF EXISTS `company`;
 CREATE TABLE `company` (
   `companyID` MEDIUMINT NOT NULL AUTO_INCREMENT,
   `companyName` varchar(50) NOT NULL,
-  `companyDescription` varchar(50) NOT NULL,
-  `location` varchar(50) NOT NULL,
-  `companyLinkedinLink` varchar(50) NOT NULL,
-  `companyWebsite` varchar(50) NOT NULL,
-  `industry` varchar(50) NOT NULL,
-  `imageLink` varchar(50) NOT NULL,
+  `companyDescription` varchar(50),
+  `companyLinkedinLink` varchar(50),
+  `companyWebsite` varchar(50),
+  `industry` varchar(50),
+  `imageLink` varchar(50),
+  `location` varchar(50),
+  -- google map api needs longitude and latitude, may need to store an attribute for each, or generate them dynamically 
 
-  `numberOfClicks` int NOT NULL,
-  `totalNumReviews` int NOT NULL,
-  `overallRating` float(5) NOT NULL,
-  `averageCriteria1` float(5) NOT NULL,
-  `averageCriteria2` float(5) NOT NULL,
-  `averageCriteria3` float(5) NOT NULL,
-  `averageCriteria4` float(5) NOT NULL,
-  `averageCriteria5` float(5) NOT NULL,
-  `averageCriteria6` float(5) NOT NULL,
+  `numberOfClicks` int,
+  `totalNumReviews` int,
+  `overallRating` float(5),
+  `averageCriteria1` float(5),
+  `averageCriteria2` float(5),
+  `averageCriteria3` float(5),
+  `averageCriteria4` float(5),
+  `averageCriteria5` float(5),
+  `averageCriteria6` float(5),
   PRIMARY KEY (`companyID`)
 );
 
