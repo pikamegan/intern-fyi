595bea101913027e35c5402cad214ea7fe366ccc
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-10-13 21:29:43 +0800

    added most of the pages templates

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,35 @@
-if (window.navigator.onLine) {
-    console.log("you are online");
-} else {
-    console.log("you are offline");
-    window.location = "../HTML/internetstopped.html"
+// if (window.navigator.onLine) {
+//     console.log("you are online");
+// } else {
+//     console.log("you are offline");
+//     window.location = "../HTML/internetstopped.html"
+// }
+//Above code is not working as expected... need more working
+
+
+function gotoWriteAReview() {
+    window.location = "../HTML/WriteAReview.html"
 }
+
+function gotoSignUpPage() {
+    window.location = "../HTML/signup.html"
+}
+
+function gotoSignInPage() {
+    window.location = "../HTML/signin.html"
+}
+
+
+function gotoHomePage() {
+    window.location = "../index.html"
+}
+
+
+function gotoMyProfile() {
+    window.location = "../HTML/myProfile.html"
+}
+
+
+
 
 
