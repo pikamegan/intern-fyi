a106b1da3aa2a85352e250f99d918caa50593fda
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-10-22 17:14:13 +0800

    changed nav bar
    changed nav bar

--- js/actions.js
+++ js/actions.js
@@ -42,5 +42,16 @@ function closeForm() {
 }
 
 
+// function navBar() {
+//     var x = document.getElementById("myLinks");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
+
+
+
 
 
