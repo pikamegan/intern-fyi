151c4b9e8bc075ebb183d31221d3a52f0813a429
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-10-22 17:49:48 +0800

    added some things

--- js/actions.js
+++ js/actions.js
@@ -42,17 +42,19 @@ function closeForm() {
 }
 
 function showCriteria() {
-    let criteria = {"Pay": "This job pays well", 
-        "Skills Learned": "The company helped me learn new skills", 
-        "Company Culture": "The company's level of cohesion and friendliness", 
-        "Food": "The price of nearby food", 
-        "Mentorship": "The company provides in-person training or mentorship", 
-        "Flat Hierarchy": "The company maintains little hierarchy"}
-    
+    let criteria = {
+        "Pay": "This job pays well",
+        "Skills Learned": "The company helped me learn new skills",
+        "Company Culture": "The company's level of cohesion and friendliness",
+        "Food": "The price of nearby food",
+        "Mentorship": "The company provides in-person training or mentorship",
+        "Flat Hierarchy": "The company maintains little hierarchy"
+    }
+
     let counter = 1
     for (criterion in criteria) {
-        document.getElementById('btn'+counter).innerHTML += `<strong>${criterion}</strong>`
-        document.getElementById('body'+counter).innerHTML += `${criteria[criterion]}`
+        document.getElementById('btn' + counter).innerHTML += `<strong>${criterion}</strong>`
+        document.getElementById('body' + counter).innerHTML += `${criteria[criterion]}`
         counter += 1
     }
 }
@@ -79,14 +81,39 @@ const navigationBar = Vue.createApp({
 
 
 //Components
-navigationBar.component('navigationBarSmallLogin', {
+navigationBar.component('navigation-bar-small-login', {
     data() {  // data option of the component
         return {
         }
     },
     methods: {
     },
-    template: ``
+    template: `<!-- SMALL navigation bar for width smaller or = to 767  (USER LOGINed)-->
+
+    <nav class="navbar navbar-expand-md navbar-light bg-white" aria-label="Navbar">
+        <div class="container-fluid d-inline-block">
+        <div class="input-group my-2">
+            <a class="navbar-brand" href="../index.html">
+            <img src="../IMG/Website-Logo.svg" style="height:40px;" onclick="gotoHomePage()">
+        </a>
+        <input type="search" class="form-control rounded rounded-2" placeholder="Search..." aria-label="Search"
+            aria-describedby="search-addon" />
+        <button type="button" class="btn btn-success" id="searchButton">
+            <div style="background-image: url(../img/search-magnifiying-glass.svg);width: 25px;height: 23px;"></div>
+        </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        </div>
+    </div>
+    <div class="collapse navbar-collapse text-center" id="navbarCollapse">
+        <a href="">Logout</a><br>
+        <a href="../HTML/myProfile.html">My Profile</a><br>
+        <a href="../HTML/WriteAReview.html">Write a Review &#9998;</a><br>
+    </div>
+    </nav>
+    <!-- end of small navigation bar -->`
 })
 
 navigationBar.component('navigationBarSmallLogout', {
@@ -96,7 +123,33 @@ navigationBar.component('navigationBarSmallLogout', {
     },
     methods: {
     },
-    template: ``
+    template: `<!-- SMALL navigation bar for width smaller or = to 767  (USER LOGOUTed)-->
+
+    <nav class="navbar navbar-expand-md navbar-light bg-white" aria-label="Navbar">
+        <div class="container-fluid d-inline-block">
+
+        <div class="input-group my-2">
+            <a class="navbar-brand" href="../index.html">
+            <img src="../IMG/Website-Logo.svg" style="height:40px;" onclick="gotoHomePage()">
+            </a>
+            <input type="search" class="form-control rounded rounded-2" placeholder="Search..." aria-label="Search"
+            aria-describedby="search-addon" />
+            <button type="button" class="btn btn-success" id="searchButton">
+            <div style="background-image: url(../img/search-magnifiying-glass.svg);width: 25px;height: 23px;"></div>
+            </button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        </div>
+        <div class="collapse navbar-collapse text-center" id="navbarCollapse">
+        <a href="../HTML/signup.html">Register</a><br>
+        <a href="../HTML/lgin.html">Login</a><br>
+        <a href="../HTML/WriteAReview.html">Write a Review &#9998;</a><br>
+    </div>
+    </nav>
+    <!-- end of small navigation bar -->`
 })
 
 navigationBar.component('navigationBarBigLogin', {
@@ -106,9 +159,36 @@ navigationBar.component('navigationBarBigLogin', {
     },
     methods: {
     },
-    template: ``
-})
+    template: `<nav class="navbar navbar-expand-md navbar-light bg-white" aria-label="Navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../index.html">
+        <img src="../IMG/Website-Logo.svg" style="height:40px;">
+        </a>
 
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="input-group my-2">
+            <input type="search" class="form-control rounded" placeholder="Search for Industy, Company or Role"
+            aria-label="Search" aria-describedby="search-addon" />
+            <button type="button" class="btn btn-success" id="searchButton">
+            <div style="background-image: url(../img/search-magnifiying-glass.svg);width: 25px;height: 23px;"></div>
+            </button>
+        </div>
+        <button class="btn btn-primary my-2 mx-4 p-1" style="width:170px;" id="reviewBtn" onclick="gotoWriteAReview()">
+            Write review
+        </button>
+        <button class="btn border-secondary rounded-circle me-2" id="userBtn" onclick="gotoMyProfile()">
+            ZT
+        </button>
+
+        </div>
+    </div>
+    </nav>`
+})
 
 navigationBar.component('navigationBarBigLogout', {
     data() {  // data option of the component
@@ -117,7 +197,38 @@ navigationBar.component('navigationBarBigLogout', {
     },
     methods: {
     },
-    template: ``
+    template: `<!-- Big Nav Bar width > 767 and Logout -->
+
+    <nav class="navbar navbar-expand-md navbar-light bg-white" aria-label="Navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../index.html">
+        <img src="../IMG/Website-Logo.svg" style="height:40px;" onclick="gotoHomePage()">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="input-group my-2">
+            <input type="search" class="form-control rounded" placeholder="Search for Industy, Company or Role"
+            aria-label="Search" aria-describedby="search-addon" />
+            <button type="button" class="btn btn-success" id="searchButton">
+            <div style="background-image: url(../img/search-magnifiying-glass.svg);width: 25px;height: 23px;"></div>
+            </button>
+        </div>
+        <button class="btn btn-secondary my-2 mx-4" id="loginBtn" onclick="gotoSignInPage()">
+            Login
+        </button>
+        <button class="btn btn-primary me-2" id="signUpBtn" onclick="gotoSignUpPage()">
+            Signup
+        </button>
+
+        </div>
+    </div>
+    </nav>
+    <!-- end of navigation bar -->`
 })
 
 navigationBar.mount('#navbarTemplate')
