0023fd92763ec5ce9b6cc5b24d27d0da8b721aa3
Megan Thong <meganthong@outlook.com>
2021-10-23 12:17:01 +0800

    scroll top button

--- js/actions.js
+++ js/actions.js
@@ -60,6 +60,23 @@ function showCriteria() {
     }
 }
 
+function scrollToTop() {
+    let scrollBtn = document.getElementById("scrollTop")
+    // adapted from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block";
+      } else {
+        scrollBtn.style.display = "none";
+      }
+    }
+
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // Vue instance
 const navigationBar = Vue.createApp({
     data() {
