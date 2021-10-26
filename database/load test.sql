6ea6aeb13dcd8025ea6bfe34484ba47f28f9578f
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-10-26 23:14:44 +0800

    changes tp myProfile

--- database/load test.sql
+++ database/load test.sql
@@ -1,91 +1,72 @@
 --
 -- Data for table `gender`
 --
-INSERT INTO 
-  gender(
-    `genderID`, 
-    `genderName`
-  )
-values
-  (
-    "M",
-    "Male"
-  ),
-  (
-    "F", 
-    "Female"
-  ), 
-  (
-    "O", 
-    "Others"
-  ); 
-
+INSERT INTO gender(`genderID`, `genderName`)
+values ("M", "Male"),
+  ("F", "Female"),
+  ("O", "Others");
 --
 -- Data for table `intern`
 --
-INSERT INTO 
-  intern(
-    `firstName`, 
-    `lastName`, 
-    `genderID`, 
-    `country`, 
-    `school`, 
-    `schoolEmail`, 
-    `password`, 
-    `profilePictureUrl`, 
+INSERT INTO intern(
+    `firstName`,
+    `lastName`,
+    `genderID`,
+    `country`,
+    `school`,
+    `schoolEmail`,
+    `password`,
+    `profilePictureUrl`,
     `reviewsNo`
   )
-values
-  (
-    "Esther", 
-    "Kim", 
-    "F", 
-    "Singapore", 
-    "Singapore Management University", 
-    "esther.2020@scis.smu.edu.sg", 
-    "Esther@123", 
-    "https://icon-library.com/images/woman-profile-icon/woman-profile-icon-3.jpg", 
+values (
+    "Esther",
+    "Kim",
+    "F",
+    "Singapore",
+    "Singapore Management University",
+    "esther.2020@scis.smu.edu.sg",
+    "Esther@123",
+    "https://icon-library.com/images/woman-profile-icon/woman-profile-icon-3.jpg",
     321
   ),
   (
-    "Megan", 
-    "Thong", 
-    "F", 
-    "Singapore", 
-    "Singapore Management University", 
-    "megan.2020@scis.smu.edu.sg", 
-    "Megan@45", 
-    "https://icon-library.com/images/woman-profile-icon/woman-profile-icon-3.jpg", 
+    "Megan",
+    "Thong",
+    "F",
+    "Singapore",
+    "Singapore Management University",
+    "megan.2020@scis.smu.edu.sg",
+    "Megan@45",
+    "https://icon-library.com/images/woman-profile-icon/woman-profile-icon-3.jpg",
     54
-  ), 
+  ),
   (
-    "Patricia", 
-    "Loi", 
-    "O", 
-    "Singapore", 
-    "Singapore Management University", 
-    "patricia.2020@scis.smu.edu.sg", 
-    "Patricia@67", 
-    "https://icon-library.com/images/woman-profile-icon/woman-profile-icon-3.jpg", 
+    "Patricia",
+    "Loi",
+    "O",
+    "Singapore",
+    "Singapore Management University",
+    "patricia.2020@scis.smu.edu.sg",
+    "Patricia@67",
+    "https://icon-library.com/images/woman-profile-icon/woman-profile-icon-3.jpg",
     76
-  ), 
+  ),
   (
-    "Tianyu", 
-    "Chen", 
-    "M", 
-    "Singapore", 
-    "Singapore Management University", 
-    "tianyu.2020@scis.smu.edu.sg", 
-    "Tianyu@90", 
-    "https://cdn5.vectorstock.com/i/1000x1000/13/04/male-profile-picture-vector-2041304.jpg", 
+    "Tianyu",
+    "Chen",
+    "M",
+    "Singapore",
+    "Singapore Management University",
+    "tianyu.2020@scis.smu.edu.sg",
+    "Tianyu@90",
+    "https://cdn5.vectorstock.com/i/1000x1000/13/04/male-profile-picture-vector-2041304.jpg",
     9
-); 
-
+  );
 --
 -- Data for table `company`
 --
-insert into
-  company(
+insert into company(
     -- `companyID`,
     `companyName`,
     `companyDescription`,
@@ -104,8 +85,7 @@ insert into
     `averageCriteria5`,
     `averageCriteria6`
   )
-values
-  (
+values (
     'Google Asia Pacific, Singapore',
     'top tech giant',
     'https://sg.linkedin.com/company/google',
@@ -266,7 +246,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Food Panda',
     'Food Delivery Service',
     'https://sg.linkedin.com/company/foodpanda',
@@ -283,7 +264,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'DHL',
     'Established in over 220 countries, employing over 100,000 people throughout over 3000 facilities, DHL Express serves over 2.6 million customers worldwide, making us the largest international express courier company in the world.',
     'https://www.linkedin.com/company/dhl',
@@ -300,7 +282,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'HP',
     'HP Inc. (NYSE: HPQ) creates technology that makes life better for everyone, everywhere. Through our product and service portfolio of personal systems, printers and 3D printing solutions, we engineer experiences that amaze.',
     'https://www.linkedin.com/company/hp',
@@ -317,7 +300,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Procter & Gamble',
     'The award-winning consumer goods giant is crowned Singapore’s top employer for 2021, and with good reason. Today, Procter & Gamble Singapore is powered by over 2,000 dedicated employees from more than 40 nationalities. An ardent believer in the vast potential of cutting-edge technology, Procter & Gamble plans to invest up to $50 million in total business expenditure in Singapore to further advance digital innovation in the country. Furthermore, it has signalled its intention to train at least 50 employees to embark on novel digital roles.',
     'https://sg.linkedin.com/company/procter-and-gamble',
@@ -334,7 +318,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Dyson',
     'The British electric appliance firm employs approximately 1,200 people in Singapore, of whom 350 are engineers and scientists.',
     'https://sg.linkedin.com/company/dyson',
@@ -351,7 +336,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Singapore Managment University',
     'A premier university in Asia, the Singapore Management University (SMU) is internationally recognised for its world-class research and distinguished teaching. Established in 2000, SMU’s mission is to generate leading-edge research with global impact and produce broad-based, creative and entrepreneurial leaders for the knowledge-based economy.',
     'https://www.linkedin.com/school/singapore-management-university/',
@@ -368,7 +354,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Dell',
     'Dell Inc., formerly PC’s Limited (1984–88) and Dell Computer Corporation (1988–2003), global company that designs, develops, and manufactures personal computers (PCs) and a variety of computer-related products. The company is one of the world’s leading suppliers of PCs. Dell is headquartered in Round Rock, Texas.',
     'https://www.linkedin.com/company/delltechnologies',
@@ -385,7 +372,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Disney',
     'The mission of The Walt Disney Company is to entertain, inform and inspire people around the globe through the power of unparalleled storytelling, reflecting the iconic brands, creative minds and innovative technologies that make ours the world’s premier entertainment company.',
     'https://www.linkedin.com/company/the-walt-disney-company-southeast-asia-pte.-ltd.',
@@ -402,7 +390,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Lazada Singapore',
     'Lazada Singapore is the leading online shopping platform in Singapore. We are always striving to keep up with what consumers want and need. We are making every effort to achieve maximum customer satisfaction through seamless transactions and competitive product pricing. We are updating and improving our product selections at the best prices to delight our customers along with great deals and flash sales.',
     'https://www.linkedin.com/company/lazada',
@@ -419,7 +408,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Schneider Electric Singapore',
     'We believe access to energy and digital is a basic human right. Our generation is facing a tectonic shift in energy transition and industrial revolution catalysed by accelerated digitisation in a more electric world. Electricity is the most efficient and best vector for decarbonisation; combined with a circular economy approach, we will achieve a climate-positive impact as part of the United Nations Sustainable Development Goals.',
     'https://sg.linkedin.com/company/schneider-electric',
@@ -436,7 +426,8 @@ values
     0.0,
     0.0,
     0.0
-  ),(
+  ),
+(
     'Shell Singapore',
     'We are a global group of energy and petrochemicals companies with an average of 86,000 employees in more than 70 countries. We use advanced technologies and take an innovative approach to help build a sustainable energy future.',
     'https://www.linkedin.com/company/shell',
@@ -453,123 +444,75 @@ values
     0.0,
     0.0,
     0.0
-  ); 
-
+  );
 --
 -- Data for table `review`
 --
-INSERT INTO 
-	review(
-    `companyID`, 
+INSERT INTO review(
+    `companyID`,
     `reviewID`,
-	  `jobTitle`,
+    `jobTitle`,
     `schoolEmail`,
-	  `reviewDescription`, 
-	  `overallRating`, 
-	  `criteria1Rating`, 
-	  `criteria2Rating`, 
-	  `criteria3Rating`, 
-	  `criteria4Rating`, 
-	  `criteria5Rating`, 
-	  `criteria6Rating`, 
-	  `totalUpvotesNo`, 
-	  `totalDownvotesNo`, 
-	  `checkSFW`
+    `reviewDescription`,
+    `overallRating`,
+    `criteria1Rating`,
+    `criteria2Rating`,
+    `criteria3Rating`,
+    `criteria4Rating`,
+    `criteria5Rating`,
+    `criteria6Rating`,
+    `totalUpvotesNo`,
+    `totalDownvotesNo`,
+    `checkSFW`
   )
-  values(
-  1,
-  1, 
-  "Marketing Intern",
-  "esther.2020@scis.smu.edu.sg",
-  "I learnt about Search Engine Optimisation (SEO) and Google Ads, which align with my Business Analysis specialisation perfectly!",
-  5, 
-  5, 
-  4.9, 
-  5, 
-  4.9, 
-  5, 
-  4.9, 
-  34, 
-  5, 
-  1
-  ), 
+values(
+    1,
+    1,
+    "Marketing Intern",
+    "esther.2020@scis.smu.edu.sg",
+    "I learnt about Search Engine Optimisation (SEO) and Google Ads, which align with my Business Analysis specialisation perfectly!",
+    5,
+    5,
+    4.9,
+    5,
+    4.9,
+    5,
+    4.9,
+    34,
+    5,
+    1
+  ),
   (
-  1,
-  2,
-  "Policy Analyst",
-  "patricia.2020@scis.smu.edu.sg",
-  "The best internship I've had.",
-  5, 
-  5, 
-  4.9, 
-  5, 
-  4.9, 
-  4.8, 
-  4.9, 
-  12, 
-  2, 
-  0
-  ); 
-
+    1,
+    2,
+    "Policy Analyst",
+    "patricia.2020@scis.smu.edu.sg",
+    "The best internship I've had.",
+    5,
+    5,
+    4.9,
+    5,
+    4.9,
+    4.8,
+    4.9,
+    12,
+    2,
+    0
+  );
 --
 -- Data for table `vote`
 --
-
-INSERT INTO
-vote(
-  `companyID`, 
-  `reviewID`, 
-  `voteID`, 
-  `isUpvote`, 
-  `isDownvote`
-)
-VALUES(
-  1,
-  1, 
-  1,
-  1,
-  0
-), 
-(
-  1, 
-  1,
-  2, 
-  1,
-  0
-), 
-(
-  1, 
-  1,
-  3, 
-  0,
-  1
-), 
-(
-  1,
-  2,
-  4,
-  1,
-  0
-), 
-(
-  1,
-  1,
-  5, 
-  0,
-  1
-), 
-(
-  1,
-  2, 
-  6,
-  1,
-  0
-), 
-(
-  1,
-  1,
-  7, 
-  1,
-  0
-); 
-
+INSERT INTO vote(
+    `companyID`,
+    `reviewID`,
+    `voteID`,
+    `isUpvote`,
+    `isDownvote`
+  )
+VALUES(1, 1, 1, 1, 0),
+  (1, 1, 2, 1, 0),
+  (1, 1, 3, 0, 1),
+  (1, 2, 4, 1, 0),
+  (1, 1, 5, 0, 1),
+  (1, 2, 6, 1, 0),
+  (1, 1, 7, 1, 0);
