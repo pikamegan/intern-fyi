b588350b08d13fc54dc30ba848a36cf3b190f661
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-10-29 20:33:06 +0800

    Update ConnectionManager.php

--- processDbRequest/model/ConnectionManager.php
+++ processDbRequest/model/ConnectionManager.php
@@ -1,8 +1,26 @@
 <?php
-class ConnectionManager {
-    public function getConnection() {        
-        $dsn  = "mysql:host=localhost;dbname=wad2g5t2intern";
-        return new PDO($dsn, "root", "");  
+class ConnectionManager
+{
+
+    public function connect()
+    {
+        $servername = 'localhost';
+        $username = 'root';
+        $password = '';
+        $dbname = '';
+        $port = '3309';
+
+        // Create connection
+        $pdoObject = new PDO(
+            "mysql:host=$servername;dbname=$dbname;port=$port",
+            $username,
+            $password);
+
+        $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // if fail, exception will be thrown
+
+        // Return connection object
+        return $pdoObject; // PDO object (containing MySQL connection info)
     }
+
 }
-?>
