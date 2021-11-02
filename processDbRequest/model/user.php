755f0ad344e799948aba59631e30bc2a5d7e43a1
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-11-02 11:21:00 +0800

    added user dao and stuff

---
+++ processDbRequest/model/user.php
@@ -0,0 +1,121 @@
+<?php
+
+class user
+{
+    private $firstName;
+    private $lastName;
+    private $genderID;
+    private $country;
+    private $school;
+    private $schoolEmail;
+    private $password;
+    private $profilePictureUrl;
+    private $reviewsNo;
+
+    public function __construct($firstName,$lastName,$genderID,$country,$school,$schoolEmail,$password,$profilePictureUrl,$reviewsNo)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->genderID = $genderID;
+        $this->country = $country;
+        $this->school = $school;
+        $this->schoolEmail = $schoolEmail;
+        $this->password = $password;
+        $this->profilePictureUrl = $profilePictureUrl;
+        $this->reviewsNo = $reviewsNo;
+    }
+
+
+
+
+    /**
+     * Get the value of firstName
+     */ 
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Get the value of lastName
+     */ 
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Get the value of genderID
+     */ 
+    public function getGenderID()
+    {
+        return $this->genderID;
+    }
+
+    /**
+     * Get the value of country
+     */ 
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Get the value of school
+     */ 
+    public function getSchool()
+    {
+        return $this->school;
+    }
+
+    /**
+     * Get the value of schoolEmail
+     */ 
+    public function getSchoolEmail()
+    {
+        return $this->schoolEmail;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the value of profilePictureUrl
+     */ 
+    public function getProfilePictureUrl()
+    {
+        return $this->profilePictureUrl;
+    }
+
+    /**
+     * Get the value of reviewsNo
+     */ 
+    public function getReviewsNo()
+    {
+        return $this->reviewsNo;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <script>
+        // let alertMsg = `Pls Login`;
+        // alert(alertMsg)
+        // window.location.assign('../../HTML/login.html')
+    </script>
+</body>
+</html>
---
+++ processDbRequest/model/userDAO.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once 'common.php';
+
+class userDAO
+{
+
+    public function getUserByEmail($userEmail)
+    {
+        // STEP 1
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+
+        // STEP 2
+        $sql = "SELECT
+                    *
+                FROM intern
+                WHERE
+                schoolEmail = :userEmail";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':userEmail', $userEmail, PDO::PARAM_STR);
+
+        // STEP 3
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        // STEP 4
+        $review_object = null;
+        if ($row = $stmt->fetch()) {
+            $user_object =
+            new review(
+                $row['firstName'],
+                $row['lastName'],
+                $row['genderID'],
+                $row['country'],
+                $row['school'],
+                $row['schoolEmail'],
+                $row['password'],
+                $row['profilePictureUrl'],
+                $row['reviewsNo']);
+        }
+
+        // STEP 5
+        $stmt = null;
+        $conn = null;
+
+        // STEP 6
+        return $user_object;
+    }
+
+    public function addUser($firstName,$lastName,$genderID,$country,$school,$schoolEmail,$password,$profilePictureUrl)
+    {
+    
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+
+        // INSERT INTO `intern`(`firstName`, `lastName`, `genderID`, `country`, `school`, `schoolEmail`, `password`, `profilePictureUrl`, `reviewsNo`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9])
+
+        $sql = "INSERT INTO `intern`(`firstName`, `lastName`, `genderID`, `country`, `school`, `schoolEmail`, `password`, `profilePictureUrl`, `reviewsNo`) VALUES (:firstName,:lastName,:genderID,:country,:school,:schoolEmail,:password,:profilePictureUrl,0)";
+        
+        // STEP 2
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':firstName', $firstName, PDO::PARAM_STR);
+        $stmt->bindParam(':lastName', $lastName, PDO::PARAM_STR);
+        $stmt->bindParam(':genderID', $genderID, PDO::PARAM_STR);
+        $stmt->bindParam(':country', $country, PDO::PARAM_STR);
+        $stmt->bindParam(':school', $school, PDO::PARAM_STR);
+        $stmt->bindParam(':schoolEmail', $schoolEmail, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+        $stmt->bindParam(':profilePictureUrl', $profilePictureUrl, PDO::PARAM_STR);
+        //STEP 3
+        $status = $stmt->execute();
+
+        // STEP 4
+        $stmt = null;
+        $conn = null;
+
+        // STEP 5
+        return $status;
+    }
+}
