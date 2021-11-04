ae9d579816e0af15c221362d64d9fd987123f1da
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-11-04 20:16:48 +0800

    changes

--- processDbRequest/model/user.php
+++ processDbRequest/model/user.php
@@ -102,20 +102,3 @@ class user
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-<body>
-    <script>
-        // let alertMsg = `Pls Login`;
-        // alert(alertMsg)
-        // window.location.assign('../../HTML/login.html')
-    </script>
-</body>
-</html>
