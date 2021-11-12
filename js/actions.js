e1c3ba2ea818c4a216c6ff7b01b552e516234804
Megan Thong <meganthong@outlook.com>
2021-11-12 13:39:39 +0800

    test img

--- js/actions.js
+++ js/actions.js
@@ -153,7 +153,7 @@ function showCriteria() {
     }
 
     let criteriaImg = [
-        '../IMG/pay_companyreview.svg', 'skills_companyreview.svg', 'companyculture_companyreview.svg', 'food_companyreview.svg',
+        'index0', '../IMG/pay_companyreview.svg', 'skills_companyreview.svg', 'companyculture_companyreview.svg', 'food_companyreview.svg',
         'mentorship_companyreview.svg', 'flathierarchy_companyreview.svg'
     ]
 
@@ -173,7 +173,7 @@ function showCriteria() {
             <div class="accordion-body px-5">
                 <div class="row mb-1 align-items-center">
                     <div class="col-12 col-md-6 justify-content-center"><img
-                            src="${criteriaImg[counter - 1]}" alt="${criterion} criteria"
+                            src="${criteriaImg[counter]}" alt="${criterion} criteria"
                             class="img-fluid d-none d-sm-block"></div>
                     <div class="col-12 col-md-6 d-flex bd-highlight flex-column mb-3">
     
