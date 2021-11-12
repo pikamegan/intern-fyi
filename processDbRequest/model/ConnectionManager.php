6d9697e406104f6472a58a1f3ba56719121ec74e
Megan Thong <meganthong@outlook.com>
2021-11-12 12:22:04 +0800

    update db connection manager

--- processDbRequest/model/ConnectionManager.php
+++ processDbRequest/model/ConnectionManager.php
@@ -4,44 +4,41 @@ class ConnectionManager
 
     public function getConnection()
     {
-        $servername = 'localhost';
-        $username = 'root';
-        // $password = 'root'; // for Mac
-        $password = '';
-        $dbname = 'wad2g5t2intern';
-        $port = '3309';
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
+
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+
+        try {
+            $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // echo "Connected successfully";
+            return $conn;
+        }
+        catch(PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+        }
 
         // Create connection
-        $pdoObject = new PDO(
-            "mysql:host=$servername;dbname=$dbname;port=$port",
-            $username,
-            $password);
+        // $pdoObject = new PDO(
+        //     "mysql:host=$servername;dbname=$dbname;port=$port",
+        //     $username,
+        //     $password);
 
-        $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // if fail, exception will be thrown
-
-        // this dont work
-        // $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+        // $pdoObject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
         // Return connection object
-        return $pdoObject; // PDO object (containing MySQL connection info)
+        // return $pdoObject; // PDO object (containing MySQL connection info)
     }
 
 }
 
-
-
-// works for tim
-
-// class ConnectionManager
-// {
-//     public function getConnection()
-//     {
-//         $dsn  = "mysql:host=localhost;dbname=wad2g5t2intern";
-//         return new PDO($dsn, "root", "");
-//     }
-// }
 
 ?>
 
