edfb32b48e788d28ed3151e9d7d514a823538ee8
Megan Thong <meganthong@outlook.com>
2021-11-12 16:41:35 +0800

    install mailgun

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit7f876ca3320ae0de3572736c72c28b76
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit7f876ca3320ae0de3572736c72c28b76', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit7f876ca3320ae0de3572736c72c28b76', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInit7f876ca3320ae0de3572736c72c28b76
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit7f876ca3320ae0de3572736c72c28b76::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire7f876ca3320ae0de3572736c72c28b76($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire7f876ca3320ae0de3572736c72c28b76($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
