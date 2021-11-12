4959b600550cda69d8cdde5511d16703bba90a69
Megan Thong <meganthong@outlook.com>
2021-11-12 12:19:06 +0800

    install composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8641d9da263a575e4f0d4d6d66afba1dbbd4f02b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8641d9da263a575e4f0d4d6d66afba1dbbd4f02b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
