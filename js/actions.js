c3e8a664a14c9656fdae160367985fc55a9dac82
Megan Thong <meganthong@outlook.com>
2021-11-13 15:07:51 +0800

    test review

--- js/actions.js
+++ js/actions.js
@@ -46,6 +46,7 @@ function validate_form() {
     if (role.length == 0) {
         roleMsg.style.display = "block"
         role.scrollIntoView({ block: "center" })
+        event.preventDefault()
     } else {
         roleMsg.style.display = "none"
     }
@@ -55,6 +56,7 @@ function validate_form() {
     if (overallRadio.length == 0) {
         overallMsg.style.display = "block"
         overallRadio.scrollIntoView({ block: "center" })
+        event.preventDefault()
     } else {
         overallMsg.style.display = "none"
     }
@@ -67,6 +69,7 @@ function validate_form() {
         if (thisRadio.value == "") {
             criteriaMsg.style.display = "block"
             criteriaRadio.scrollIntoView({ block: "center" })
+            event.preventDefault()
         } else {
             criteriaMsg.style.display = "none"
         }
