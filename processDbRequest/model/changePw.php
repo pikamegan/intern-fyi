2a383130a5f18f7e4f376fc6a86d71bacfeff00d
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-11-14 14:32:40 +0800

    changes

--- processDbRequest/model/changePw.php
+++ processDbRequest/model/changePw.php
@@ -4,30 +4,35 @@ session_start();
 require_once 'common.php';
 
 $userDAO = new userDAO();
-
+$_SESSION["PWdunmatch"] = "";
 
 if (isset($_POST['pwOne']) && isset($_POST['pwTwo'])) {
     $pw1 = $_POST['pwOne'];
     $pw2 = $_POST['pwTwo'];
     $email = '';
     if ($pw1 === $pw2) {
-        var_dump($_POST);
 
         if (isset($_SESSION['email'])) {
             $email = $_SESSION['email'];
         }
 
-        if ($userDAO->updatePassword($email, $pw1)){
-            window.location.assign('./signout.php');
-            exit();
+        $status = $userDAO->updatePassword($email, $pw1);
+
+        if ($status){
+            alert("Password successfully changed! Please sign in again :)");
+            header("Location: ./signout.php");
+            exit;
         
-        } 
+        } else{
+            alert("Error in changing password");
+            header("Location: ../../HTML/updatePw.php");
+            exit;
+        }
         
     }else{
         $_SESSION["PWdunmatch"] = "<p class='text-danger m-1 text-center' style='font-size: small; display: block;'>Please ensure your passwords match</p>";
-        window.location.assign('../../HTML/myProfile.php');
-        exit();
-
+        header("Location: ../../HTML/updatePw.php");
+        exit;
     }
 }
 
