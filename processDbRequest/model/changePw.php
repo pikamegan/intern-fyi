4f2e79211434f1cc0ad86c19923a70d440e5c0c7
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2021-11-14 22:43:48 +0800

    Update changePw.php

--- processDbRequest/model/changePw.php
+++ processDbRequest/model/changePw.php
@@ -19,11 +19,15 @@ if (isset($_POST['pwOne']) && isset($_POST['pwTwo'])) {
     if($pw1 !== $pw2) {
         $_SESSION["errorPWmismatch"] = " <p class='text-center text-danger m-1' style='display: none;' id='overallMsg'>Password Mismatch!</p>";
         $noErrors = false;
+        header("Location: ../../HTML/updatePw.php");
+        exit;
     }
 
     if(strlen($pw1) < 8 || strlen($pw2) < 8) {
         $_SESSION["errorLength"] = " <p class='text-center text-danger m-1' style='display: none;' id='overallMsg'>Password length must be as least 8 characters</p>";
         $noErrors = false;
+        header("Location: ../../HTML/updatePw.php");
+        exit;
     }
 
     if (isset($_SESSION['email'])) {
