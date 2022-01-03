280351cd8b2e2ceb104ca535123c0f1dd6e5f8d1
Megan Thong <meganthong@outlook.com>
2022-01-03 23:15:27 +0800

    Update appSearch.js

--- js/appSearch.js
+++ js/appSearch.js
@@ -1,5 +1,5 @@
 // uncomment for testing / deploying
-const apiKey = getenv('MAPS_API');
+const apiKey =  "<?php getenv('MAPS_API'); ?>";
 const appSearch = Vue.createApp({
     data() {
         return {
